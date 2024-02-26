b1996c903087f049a5dd5b4b81af9e4af1bc1cb0
nosba0957 <nosba0957@gmail.com>
2024-02-26 13:50:16 +0800

    Implementation q_remove_head, q_remove_tail
    We check there has at least one node in this queue so that
    list_first_entry and list_last_entry won't access invalid memory.
    strncpy() only copy bufsize - 1 because we needs to reserve for null
    character.
    At the end of function, we don't free the memory used by the node
    because we just remove it from queue, not delete it from queue.
    
    "Delete" v.s "Remove": https://english.stackexchange.com/questions/52508/difference-between-delete-and-remove

--- queue.c
+++ queue.c
@@ -66,13 +66,27 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *node = list_first_entry(head, element_t, list);
+
+    if (sp)
+        strncpy(sp, node->value, bufsize - 1);
+    list_del(head->next);
+    return node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *node = list_last_entry(head, element_t, list);
+
+    if (sp)
+        strncpy(sp, node->value, bufsize - 1);
+    list_del(head->prev);
+    return node;
 }
 
 /* Return number of elements in queue */
