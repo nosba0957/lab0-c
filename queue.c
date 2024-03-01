466754190b7854dfefbd781d11934f90d7f51f1f
nosba0957 <nosba0957@gmail.com>
2024-03-01 21:23:06 +0800

    Fix string error in q_remove_head and remove_tail
    Previous implementation has string error because we didn't put null
    character to the end of string.

--- queue.c
+++ queue.c
@@ -71,8 +71,10 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
         return NULL;
     element_t *node = list_first_entry(head, element_t, list);
 
-    if (sp)
+    if (sp) {
         strncpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
     list_del(head->next);
     return node;
 }
@@ -84,8 +86,10 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
         return NULL;
     element_t *node = list_last_entry(head, element_t, list);
 
-    if (sp)
+    if (sp) {
         strncpy(sp, node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
     list_del(head->prev);
     return node;
 }
