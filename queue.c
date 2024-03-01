07de54eb79fdf820ffceb66a79ed5437f69fd563
nosba0957 <nosba0957@gmail.com>
2024-03-01 10:18:25 +0800

    Implementation of q_sort
    Implement q_sort with merge sort. First, we break the link which
    last element connect to head. Next step, we start doing merge sort and
    also break the link from middle node because that easier to determine
    the end of list. In the merge_sort(), we use fast and slow pointer
    method to find the middle node. After recursion finished, we merge list
    with  merge_two_lists(). When the list is sorted, it has to reconstruct
    the connection we breaked before.
    merge_two_lists() use pointer to pointer that point to the head of
    result list. We can add a node to this list without extra operation. In
    the end of merge_two_lists function, we concatenate the remaining nodes
    to result list by using bitwise OR because one of "L" and "R" is NULL.
    The result of bitwise OR is the pointer of "L" or "R".

--- queue.c
+++ queue.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -170,8 +171,64 @@ void q_reverseK(struct list_head *head, int k)
     }
 }
 
+static struct list_head *merge_two_lists(struct list_head *L,
+                                         struct list_head *R)
+{
+    struct list_head *head = NULL;
+    struct list_head **ptr = &head;
+
+    for (; L && R; ptr = &(*ptr)->next) {
+        element_t *left = list_entry(L, element_t, list);
+        element_t *right = list_entry(R, element_t, list);
+        if (strcmp(left->value, right->value) > 0) {
+            *ptr = R;
+            R = R->next;
+        } else {
+            *ptr = L;
+            L = L->next;
+        }
+    }
+    *ptr = (struct list_head *) ((uintptr_t) R | (uintptr_t) L);
+    return head;
+}
+
+static struct list_head *merge_sort(struct list_head *head)
+{
+    if (!head || !head->next)
+        return head;
+    struct list_head *slow = head, *fast = head;
+
+    while (fast && fast->next) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    struct list_head *mid = slow;
+    slow->prev->next = NULL;
+
+    struct list_head *left = merge_sort(head);
+    struct list_head *right = merge_sort(mid);
+
+    return merge_two_lists(left, right);
+}
+
 /* Sort elements of queue in ascending/descending order */
-void q_sort(struct list_head *head, bool descend) {}
+void q_sort(struct list_head *head, bool descend)
+{
+    if (list_empty(head))
+        return;
+
+    head->prev->next = NULL;
+    head->next = merge_sort(head->next);
+    struct list_head *prev = head, *iter = head->next;
+    for (; iter; iter = iter->next) {
+        iter->prev = prev;
+        prev = iter;
+    }
+    prev->next = head;
+    head->prev = prev;
+    if (descend)
+        q_reverse(head);
+}
 
 /* Remove every node which has a node with a strictly less value anywhere to
  * the right side of it */
